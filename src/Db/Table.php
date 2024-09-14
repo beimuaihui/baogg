179439fb0a1fb93f37427fe75b8d26eae0d322fc
bao <beimuaihui@126.com>
2024-09-14 17:26:08 +0800

    feat: 🎸 fixed data mask

--- src/Db/Table.php
+++ src/Db/Table.php
@@ -1215,7 +1215,7 @@ class Table
         for ($i = $begin; $i <= $end; $i++) {
             $strarr[$i] = $re;
         }
-        if ($begin >= $end || $begin >= $last || $end > $last) {
+        if ($begin > $end || $begin > $last || $end > $last) {
             return '*';
         }
         return implode('', $strarr);
